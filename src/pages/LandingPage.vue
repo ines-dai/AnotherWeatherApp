9b7e9e22f45e6ec011107e05ab459c201a48abe7
ines-dai <inesdai59@gmail.com>
2023-05-18 20:10:11 +0200

    feat: initiated projet, added wireframe and started coding layout of landing page

---
+++ src/pages/LandingPage.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="mainLayout">
+    <section class="top">
+
+    </section>
+    <section class="middle">
+
+    </section>
+    <section class="bottom"></section>
+  </div>
+</template>
+
+<script>
+
+</script>
+
+<style lang="scss" scoped>
+
+.mainLayout {
+  display: flex;
+  flex-direction: column;
+  min-width: 800px;
+  width: 85%;
+  max-width: 1000px;
+  background-color: rgb(225, 225, 244);
+  border-radius: 2rem;
+}
+.top {
+  height: 5rem;
+}
+
+.middle {
+  height: 20rem;
+}
+
+.bottom {
+  height: 10rem;
+}
+</style>
--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,25 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  resolve: {
+    extensions: [".mjs", ".js", ".ts", ".jsx", ".tsx", ".json", ".vue", ".ttf"],
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+      '@comp': path.resolve(__dirname, './src/components'),
+    },
+  },
+  css: {
+    preprocessorOptions: {
+      scss: {
+        additionalData: `
+          @import "./src/styles/_variables.scss";
+          @import "./src/styles/_mixins.scss";
+        `
+      }
+    }
+  }
 })
